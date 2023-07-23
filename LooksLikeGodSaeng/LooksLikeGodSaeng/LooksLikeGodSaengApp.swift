f31e5203e563260e1488a5e9e05fcc1d6ab4b815
HanGyeongjun <030212hgjun@gmail.com>
2023-07-23 13:10:24 +0900

    [Init] #1 - Create iOS Project

---
+++ LooksLikeGodSaeng/LooksLikeGodSaeng/LooksLikeGodSaengApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooksLikeGodSaengApp.swift
+//  LooksLikeGodSaeng
+//
+//  Created by HanGyeongjun on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LooksLikeGodSaengApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
